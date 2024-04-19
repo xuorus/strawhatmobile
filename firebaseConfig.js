b07c31f12e981a56a3441cc869e841bfa5e9a5ba
Xuorus <ronanreilcatindig@gmail.com>
2024-04-19 17:22:47 +0800

    GGZ

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app'
 import {getDatabase} from 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBX0YVoCovCWY8-fWQrOWkceGrUPM3JD2s",
-    authDomain: "quickgpa-6ba75.firebaseapp.com",
-    databaseURL: "https://quickgpa-6ba75-default-rtdb.firebaseio.com",
-    projectId: "quickgpa-6ba75",
-    storageBucket: "quickgpa-6ba75.appspot.com",
-    messagingSenderId: "500711679623",
-    appId: "1:500711679623:web:2f648d91f9f835c410ba22"
+    apiKey: "AIzaSyCVu6OtQSlnJzRI6gcDnlnj3QbWX6BY01U",
+    authDomain: "strawhatcoders-c1028.firebaseapp.com",
+    databaseURL: "https://strawhatcoders-c1028-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "strawhatcoders-c1028",
+    storageBucket: "strawhatcoders-c1028.appspot.com",
+    messagingSenderId: "1073430107906",
+    appId: "1:1073430107906:web:9b732ca20a0b1ad436941b"
 };
 
 if (firebase.apps.length === 0)
